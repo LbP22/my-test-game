04ad818061ccb28a28c2f9fde506338c4a382110
LbP22 <aakopyan@devforth.io>
2023-11-15 09:29:52 +0200

    init example app with VUE

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import "./js/pixel.min.js"
+import "./js/game.js"
+</script>
+
+<template>
+    <div id="game_container" style="width: 800px; height: 600px;"></div>
+</template>
+
+<style scoped>
+</style>
--- frontend/src/js/pixel.js
+++ frontend/src/js/pixel.js
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see http://www.gnu.org/licenses/.
 
-var PixelJS = {
+export var PixelJS = {
     AnimatedSprite: function () { },
     Asset: function () { },
     Engine: function () { },
